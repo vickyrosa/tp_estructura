1c459edb723e4b00d5d3969f5e6d65b15abcc6e8
Foyarzabal <foyarzabal@itba.edu.ar>
2023-11-02 19:03:09 -0300

    hice write

--- hotel/lista_reservas.py
+++ hotel/lista_reservas.py
@@ -1,3 +1,5 @@
+import datetime
+
 class Lista_Reservas:   #Creamos una lista enlazada de las reservas (Gran volumen de datos)
     def __init__(self):
         self.cabeza = None
@@ -48,13 +50,16 @@ class Lista_Reservas:   #Creamos una lista enlazada de las reservas (Gran volume
     def len_lista(self):
         return self.tamanio
     
-    def historico_general_reservas(self):  #FALTAAAAAAAAA
-        historico_gral_reservas = open('tp_estructura/txt/historico_gral_reservas.txt', 'a')
-        for reserva in historico_gral_reservas:
-            pass
-        
-        historico_gral_reservas.write(f'\n')
+    #Guarda en un archivo txt las reservas que se efectuaron 
+    def historico_general_reservas(self):
+        fecha_actual = datetime.date.today()
+        actual = self.cabeza
+        historico_gral_reservas = open('tp_estructura/txt/historico_gral_reservas.txt', 'w')
+        while actual:
+            if actual.fec_checkout < fecha_actual: 
+                historico_gral_reservas.write(f'{self.nroreserva}, {self.cliente}, {self.habitacion}, {self.fec_checkin}, {self.fec_checkout} \n' )
         historico_gral_reservas.close()
         
+
 
     
