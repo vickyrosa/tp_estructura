465785386a25a25799a0f342fb2685c8b77d21f3
vickyrosa <vrosa@itba.edu.ar>
2023-11-02 19:12:10 -0300

    habia hecho mal lo de set

--- usuario/cliente.py
+++ usuario/cliente.py
@@ -7,11 +7,5 @@ class Cliente(Usuario):
     def __init__(self, tipo_usuario, dni, nombre, contra, edad, sexo, telefono, mail, domicilio, fec_alta, historico_gastos):
         super().__init__(tipo_usuario, dni, nombre, contra, edad, sexo, telefono, mail, domicilio, fec_alta)
         self.historico_gastos = historico_gastos
-
-    def agregar_dni(self, dni):
         self.set_dni.add(dni)
-
-    def mostrar_dnis(self):
-        for dni in self.set_dni:
-            print(dni)
     
