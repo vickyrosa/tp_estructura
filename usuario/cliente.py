f8e5b3a2293ef101b4958b41cae17f02fb7332d1
vickyrosa <vrosa@itba.edu.ar>
2023-11-02 19:05:43 -0300

    cambie lo de set de dni

--- usuario/cliente.py
+++ usuario/cliente.py
@@ -1,6 +1,17 @@
 from usuario.usuario import Usuario
 
 class Cliente(Usuario):
+    
+    set_dni= {}
+
     def __init__(self, tipo_usuario, dni, nombre, contra, edad, sexo, telefono, mail, domicilio, fec_alta, historico_gastos):
         super().__init__(tipo_usuario, dni, nombre, contra, edad, sexo, telefono, mail, domicilio, fec_alta)
         self.historico_gastos = historico_gastos
+
+    def agregar_dni(self, dni):
+        self.set_dni.add(dni)
+
+    def mostrar_dnis(self):
+        for dni in self.set_dni:
+            print(dni)
+    
